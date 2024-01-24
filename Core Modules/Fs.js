df38f5d297d8362d055fe50d7cfbc600cf828685
FahadShaik <shaikfahad904@gmail.com>
2024-01-24 09:48:56 +0530

    file sysyem async and sync methods demo

--- Core Modules/Fs.js
+++ Core Modules/Fs.js
@@ -3,3 +3,12 @@ const fsModule = require("fs");
 // to read the data synchronously
 let data = fsModule.readFileSync("dummy.txt", "utf-8");
 console.log(data);
+
+// to read the data asynchronously
+fsModule.readFile("dummy.txt", "utf-8", (err, data) => {
+  //reading and adding the same data to the new file
+  fsModule.writeFile("newData.txt", data, (error) => {
+    if (error) throw error;
+    console.log(data, "success");
+  });
+});
