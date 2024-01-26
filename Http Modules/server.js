09943c82d0f45ad5aa8ff9e5ba2dd1320c2a7313
FahadShaik <shaikfahad904@gmail.com>
2024-01-26 09:27:47 +0530

    one-liner for the starting of dummy server using node js module

--- Http Modules/server.js
+++ Http Modules/server.js
@@ -1,10 +1,13 @@
-//Imported the Http module
+// first way to start the server
+
+/* //Imported the Http module
 const httpModule = require("http");
 
 // now lets create a server
 
 const server = httpModule.createServer((request, response) => {
-  response.end("Hello, http server");
+  response.end(`<h1>hello Http server running.css
+  jee</h1>`);
 });
 
 // For the server we need to host on somewhere on a particular address to listen the details
@@ -13,3 +16,14 @@ server.listen(1900, (err) => {
   if (err) throw err;
   console.log("Hosted the server successfully");
 });
+ */
+
+// one liner to start the server
+
+require("http")
+  .createServer((request, response) => {
+    response.end(`<h1>good to go</h1>`);
+  })
+  .listen(9000, () => {
+    console.log("server running successfully");
+  });
