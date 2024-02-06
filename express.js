b0bced91b48821ac792d779af34b8df046838882
FahadShaik <shaikfahad904@gmail.com>
2024-02-06 10:04:18 +0530

    Developing api using express JS

--- express.js
+++ express.js
@@ -3,9 +3,44 @@ let express = require("express");
 // initialize the app(Step 1)
 let app = express();
 
+/**
+ *API_URL: localhost:5000/
+ Method: GET
+ Fields: Not Required
+ Access Type: Public
+ */
+
 //Routing
 app.get("/", (req, res) => {
   res.send("Root request");
+});
+/**
+ *API_URL: localhost:5000/user
+ Method: GET
+ Fields: Not Required
+ Access Type: Public
+ */
+app.get("/user", (req, res) => {
+  res.send("User data");
+});
+/**
+ *API_URL: localhost:5000/employee
+ Method: GET
+ Fields: Not Required
+ Access Type: Public
+ */
+app.get("/employee", (req, res) => {
+  res.send("Employee data");
+});
+/**
+ *API_URL: localhost:5000/employee/details
+ Method: GET
+ Fields: Not Required
+ Access Type: Public
+ */
+
+app.get("/employee/details", (req, res) => {
+  res.send("Employee Details data");
 });
 
 //Creating the server for that app
